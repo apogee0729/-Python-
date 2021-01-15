a1dc4bc53bccef5171f02d24ee77c2b76f80c2ca
Apogee Syu <apogee0729@gmail.com>
2021-01-15 23:49:36 +0800

    2021-01-15

---
+++ 23_Input.py
@@ -0,0 +1,10 @@
+# title    : Input(請使用者輸入)
+# file     : 23_Input.py
+# date     : 2021-01-15
+# author   : Apogee Syu
+# email    : apogee0729@gmail.com
+# GitHub   : https://github.com/apogee0729
+# Encoding : UTF-8
+
+name = input("請輸入你的名字： ")
+print("嗨！", name)
